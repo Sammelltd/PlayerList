300098e9214326cbc7685f1da36cd49be412c0bd
John P Shepherd <john@jpshepherd.co.uk>
2019-03-09 19:35:58 +0000

    Initial Commit

---
+++ PlayerList/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlayerList
+//
+//  Created by Johni7 on 09/03/2019.
+//  Copyright © 2019 Sammel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
